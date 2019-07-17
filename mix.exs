d93150984510e3adf82cfdf5423d8a8efc6d0034
qgadrian <qgadrian@users.noreply.github.com>
2019-07-17 20:06:47 +0200

    Bump deps

--- mix.exs
+++ mix.exs
@@ -62,7 +62,7 @@ defmodule GitHooks.MixProject do
       {:blankable, "~> 1.0.0"},
       {:credo, "~> 1.1.0", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0.0-rc.6", only: [:dev], runtime: false},
-      {:ex_doc, "~> 0.20", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false},
       {:excoveralls, "~> 0.10", only: :test},
       {:inch_ex, ">= 0.0.0", only: :docs},
       {:recase, "~> 0.6.0"}
