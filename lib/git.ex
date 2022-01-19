679201c030a268fd0c12e8b2a0b4acbc63332092
Michael J. Cohen <mjc@kernel.org>
2022-01-19 11:26:10 -0700

    Use git to find .git or fall back to old behavior

--- lib/git.ex
+++ lib/git.ex
@@ -5,25 +5,59 @@ defmodule GitHooks.Git do
 
   @doc false
   @spec resolve_git_path() :: any
-  def resolve_git_path do
-    :git_hooks
-    |> Application.get_env(:git_path)
-    |> case do
-      nil ->
-        path = Path.join(Project.deps_path(), "/../.git")
+  def resolve_git_path,
+    do: maybe_preconfigured(Application.get_env(:git_hooks, :git_path, :detect))
 
-        if File.dir?(path) do
-          path
-        else
-          resolve_git_submodule_path(path)
-        end
+  @spec maybe_preconfigured(:detect | binary) :: Path.t() | no_return
+  def maybe_preconfigured(:detect) do
+    # if we can't figure out cwd, we can't do anything
+    working_directory = working_dir(File.cwd!())
 
-      custom_path ->
-        custom_path
+    case path_from_git(working_directory) do
+      {:ok, path} ->
+        path
+
+      {:error, :no_system_git} ->
+        use_legacy_configuration()
     end
   end
 
-  @spec resolve_git_submodule_path(String.t()) :: any
+  def maybe_preconfigured(path) do
+    if File.exists?(path) do
+      path
+    else
+      # should this warn and try to detect?
+      raise "failed to find #{path}"
+    end
+  end
+
+  @spec use_legacy_configuration :: Path.t() | no_return
+  def use_legacy_configuration do
+    path = Path.join(Project.deps_path(), "/../.git")
+
+    maybe_submodules(File.dir?(path), path)
+  end
+
+  @spec path_from_git(Path.t(), String.t()) :: {:ok, Path.t()} | {:error, :no_system_git}
+  def path_from_git(wd, git \\ "git") do
+    case System.find_executable(git) &&
+           System.cmd(git, ["rev-parse", "--show-toplevel"], cd: wd) do
+      {output, 0} ->
+        # We assume git is correct about this path
+        {:ok, Path.join(String.trim(output), ".git")}
+
+      _ ->
+        {:error, :no_system_git}
+    end
+  end
+
+  defp maybe_submodules(true, path), do: resolve_git_submodule_path(path)
+
+  defp maybe_submodules(false, path), do: path
+
+  defp parent_basename(path), do: Path.basename(Path.dirname(path))
+
+  @spec resolve_git_submodule_path(String.t()) :: Path.t() | no_return
   defp resolve_git_submodule_path(git_path) do
     with {:ok, contents} <- File.read(git_path),
          %{"dir" => submodule_dir} <- Regex.named_captures(~r/^gitdir:\s+(?<dir>.*)$/, contents) do
@@ -32,6 +66,14 @@ defmodule GitHooks.Git do
     else
       _error ->
         raise "Error resolving git submodule path '#{git_path}'"
+    end
+  end
+
+  defp working_dir(path) do
+    if parent_basename(path) == "deps" do
+      Path.dirname(path)
+    else
+      path
     end
   end
 end
