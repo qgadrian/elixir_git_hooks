b164fa1ee155de3d892fb5826e6655f15a493d02
qgadrian <qgadrian@users.noreply.github.com>
2022-02-01 18:45:37 +0100

    Add pre-release for `v0.6.6`

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule GitHooks.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/qgadrian/elixir_git_hooks"
-  @version "0.6.5"
+  @version "0.6.6-pre"
 
   def project do
     [
