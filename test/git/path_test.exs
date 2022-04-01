c3927329819516c4bd892940ca9f06eb747056d2
qgadrian <qgadrian@users.noreply.github.com>
2022-04-01 22:26:03 +0200

    Use `.git` path relative to parent project path
    If the `project_path` configuration is present, the `.git` path will be
    relative to the absolute path given in the configuration.

--- test/git/path_test.exs
+++ test/git/path_test.exs
@@ -4,14 +4,29 @@ defmodule GitHooks.Git.PathTest do
   alias GitHooks.Git.Path
 
   describe "git_hooks_path_for/1" do
-    test "appends the path to the hooks folder" do
-      assert Path.git_hooks_path_for("/testing") == ".git/hooks/testing"
+    setup [:project_path]
+
+    test "appends the path to the `.git/hooks` folder", %{project_path: project_path} do
+      assert Path.git_hooks_path_for("/testing") == "#{project_path}/.git/hooks/testing"
     end
   end
 
   describe "resolve_git_hooks_path/0" do
-    test "returns the git path of the project" do
-      assert Path.resolve_git_hooks_path() == ".git/hooks"
+    setup [:project_path]
+
+    test "returns the git path of the project", %{project_path: project_path} do
+      assert Path.resolve_git_hooks_path() == "#{project_path}/.git/hooks"
     end
+  end
+
+  #
+  # Private functions
+  #
+
+  defp project_path(_) do
+    {project_path, 0} = System.cmd("pwd", [])
+    project_path = String.replace(project_path, ~r/\n/, "")
+
+    {:ok, %{project_path: project_path}}
   end
 end
