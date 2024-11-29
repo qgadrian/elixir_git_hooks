b096016ecd50fa7d00c5c985af05538a85b74855
Adrian Quintas <qgadrian@users.noreply.github.com>
2024-11-29 11:18:07 +1100

    Cleanup tmp git folder after tests
    The OS automatically cleans up that folder, but it takes no effort
    on removing them after the test suite.

--- test/support/git_project_case.ex
+++ test/support/git_project_case.ex
@@ -1,7 +1,7 @@
 defmodule GitHooks.TestSupport.GitProjectCase do
   @moduledoc """
   This module provides a setup that creates a tmp git project 
-  for the testing context
+  for the testing context.
   """
 
   use ExUnit.CaseTemplate
@@ -9,17 +9,17 @@ defmodule GitHooks.TestSupport.GitProjectCase do
   using do
     quote do
       setup do
-        # Create a temporary directory
         tmp_dir = Path.join(System.tmp_dir!(), "git_hooks_test_#{:os.system_time(:millisecond)}")
         File.mkdir_p!(tmp_dir)
 
-        # Initialize a git repository in the temporary directory
         System.cmd("git", ["init"], cd: tmp_dir)
 
-        # Set the :project_path in Application environment
         Application.put_env(:git_hooks, :project_path, tmp_dir)
 
-        # Return the context with the tmp_dir
+        on_exit(fn ->
+          File.rm_rf(tmp_dir)
+        end)
+
         {:ok, tmp_dir: tmp_dir}
       end
     end
