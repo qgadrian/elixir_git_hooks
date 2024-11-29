179357ea61e0621ac53a4528b31a7cd9c820b417
Adrian Quintas <qgadrian@users.noreply.github.com>
2024-11-29 12:55:39 +1100

    Add regression tests for fetching the git path

--- test/git/path_test.exs
+++ test/git/path_test.exs
@@ -4,6 +4,8 @@ defmodule GitHooks.Git.PathTest do
 
   alias GitHooks.Git.GitPath
 
+  @tag capture_log: true
+
   describe "git_hooks_path_for/1" do
     test "appends the path to the `.git/hooks` folder", %{tmp_dir: project_path} do
       assert GitPath.git_hooks_path_for("/testing") == "#{project_path}/.git/hooks/testing"
@@ -13,6 +15,26 @@ defmodule GitHooks.Git.PathTest do
   describe "resolve_git_hooks_path/0" do
     test "returns the git path of the project", %{tmp_dir: project_path} do
       assert GitPath.resolve_git_hooks_path() == "#{project_path}/.git/hooks"
+    end
+  end
+
+  describe "resolve_app_path/0" do
+    test "returns the project root when called from the project directory", %{
+      tmp_dir: project_path
+    } do
+      assert GitPath.resolve_app_path() == project_path
+    end
+
+    test "returns the project root when called from the dependency directory", %{
+      tmp_dir: project_path
+    } do
+      # Simulates being in the dependency directory
+      deps_git_hooks_dir = Path.join([project_path, "deps", "git_hooks"])
+      File.mkdir_p!(deps_git_hooks_dir)
+
+      File.cd!(deps_git_hooks_dir, fn ->
+        assert GitPath.resolve_app_path() == project_path
+      end)
     end
   end
 end
