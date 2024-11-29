75cfe19285402fbfc5a635c877147fadd2b8701c
Adrian Quintas <qgadrian@users.noreply.github.com>
2024-11-29 11:06:42 +1100

    Add test support module to setup a tmp git project
    This project relies 100% on git, so makes sense that the tests are
    running within a git folder. This setup created a git project in
    the system's /tmp folder.

--- test/git/path_test.exs
+++ test/git/path_test.exs
@@ -1,32 +1,18 @@
 defmodule GitHooks.Git.PathTest do
   use ExUnit.Case
+  use GitHooks.TestSupport.GitProjectCase
 
-  alias GitHooks.Git.Path
+  alias GitHooks.Git.GitPath
 
   describe "git_hooks_path_for/1" do
-    setup [:project_path]
-
-    test "appends the path to the `.git/hooks` folder", %{project_path: project_path} do
-      assert Path.git_hooks_path_for("/testing") == "#{project_path}/.git/hooks/testing"
+    test "appends the path to the `.git/hooks` folder", %{tmp_dir: project_path} do
+      assert GitPath.git_hooks_path_for("/testing") == "#{project_path}/.git/hooks/testing"
     end
   end
 
   describe "resolve_git_hooks_path/0" do
-    setup [:project_path]
-
-    test "returns the git path of the project", %{project_path: project_path} do
-      assert Path.resolve_git_hooks_path() == "#{project_path}/.git/hooks"
+    test "returns the git path of the project", %{tmp_dir: project_path} do
+      assert GitPath.resolve_git_hooks_path() == "#{project_path}/.git/hooks"
     end
-  end
-
-  #
-  # Private functions
-  #
-
-  defp project_path(_) do
-    {project_path, 0} = System.cmd("pwd", [])
-    project_path = String.replace(project_path, ~r/\n/, "")
-
-    {:ok, %{project_path: project_path}}
   end
 end
