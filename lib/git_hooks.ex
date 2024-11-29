7171e514fce5b614402972e447b1b8b2f086ce51
Adrian Quintas <qgadrian@users.noreply.github.com>
2024-11-29 12:48:01 +1100

    Use compile env to run install mix task
    This ensures that if the :auto_install configuration changes,
    the module will recompile, and the hooks will be installed or
    skipped accordingly.

--- lib/git_hooks.ex
+++ lib/git_hooks.ex
@@ -3,7 +3,9 @@ defmodule GitHooks do
 
   alias Mix.Tasks.GitHooks.Install
 
-  if Application.get_env(:git_hooks, :auto_install, true) do
+  @auto_install Application.compile_env(:git_hooks, :auto_install, true)
+
+  if @auto_install do
     Install.run(["--quiet"])
   end
 
